94901990e5c9f24be2147b8c0443f50d23c6eff8
Raphael Ladinig <ra.ladinig@gmail.com>
2025-03-14 16:11:10 +0100

    gamescope

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -1,8 +1,15 @@
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
   };
 }
