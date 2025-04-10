73997e2fb0ee5169d88a148e5b20875fb2cd9d81
Raphael Ladinig <ra.ladinig@gmail.com>
2025-04-10 10:00:12 +0200

    nvim: exrc

--- home/common/neovim/init.lua
+++ home/common/neovim/init.lua
@@ -1,3 +1,5 @@
+vim.opt.exrc = true
+
 local function load_files(path)
   if vim.fn.isdirectory(path) == 1 then
     local files = vim.fn.readdir(path)
@@ -10,22 +12,7 @@ local function load_files(path)
   end
 end
 
-local function load_local_config(path)
-  local fullpath = vim.fn.getcwd() .. "/" .. path
-  if vim.fn.filereadable(fullpath) == 1 then
-    local file_content = vim.secure.read(fullpath)
-    load(file_content)()
-    print("loaded " .. path)
-  end
-end
-
 package.path = package.path .. ";" .. vim.fn.stdpath("config") .. "/lua/modules/?.lua"
 
 load_files(vim.fn.stdpath("config") .. "/lua")
 load_files(vim.fn.stdpath("config") .. "/lua/plugins")
-
-load_local_config(".nvim.lua")
-load_local_config(".nvim/init.lua")
-
--- BUG printing is bugged
-print("")
